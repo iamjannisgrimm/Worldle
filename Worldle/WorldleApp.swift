136f055c8b7a6173475fdcd088162c56296423e6
Jannis <jannis@grimm.me>
2025-08-16 07:22:48 -0400

    Initial Commit

---
+++ Worldle/WorldleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldleApp.swift
+//  Worldle
+//
+//  Created by Jannis Grimm on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WorldleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
